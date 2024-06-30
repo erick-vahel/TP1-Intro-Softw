2360bfb9a6ac0372fb7819cde90184c50fe47fd7
Erick <npaiva@fi.uba.ar>
2024-06-30 20:04:22 -0300

    rutas creadas y crud terminado
    solo se reviso para el uso minimo

--- server/app/routes.py
+++ server/app/routes.py
@@ -10,47 +10,82 @@ import pytz
 
 # app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.errorhandler(404)
+def not_found(error):
     return """
     <html>
     <body>
-    <h1>Welcome to my heroes API</h1>
-    <a href="/characters">Go to all characters</a>
+    <h1>Ruta no encontrada</h1>
     </body>
     </html>
     """
 
-@app.route('/inicio', methods=['GET', 'POST'])
-def example_route():
-    if request.method == 'POST':
-        data = request.json
-        print(data)
-        new_entry = Usuarios(nombre=data['nombre'])
-        db.session.add(new_entry)
-        db.session.commit()
-        return jsonify({'message': 'New entry created'}), 201
 
-    elif request.method == 'GET':
-        entries = Usuarios.query.all()
-        user_list = []
-        target_timezone = pytz.timezone('America/Argentina/Buenos_Aires') 
-        for user in entries:
-            # Convertir y formatear la fecha de registro
-            if user.fecha_registro:
-                utc_date = user.fecha_registro.replace(tzinfo=pytz.utc)
-                localized_date = utc_date.astimezone(target_timezone)
-                fecha_formateada = localized_date.strftime('%Y-%m-%d %H:%M:%S')
-            else:
-                fecha_formateada = None
-            
-            user_data = {
-                'id': user.id,
-                'nombre': user.nombre,
-                'monedas': user.monedas,
-                'fecha_registro': fecha_formateada
-            }
-            user_list.append(user_data)
+@app.route('/registro', methods=['POST'])
+def registro():
+    data = request.json
+    new_entry = Usuarios(nombre=data['nombre'])
+    db.session.add(new_entry)
+    db.session.commit()
+    return jsonify({'message': 'New entry created'}), 201
+
+
+
+@app.route('/perfiles', methods=['GET'])
+def perfiles():
+    entries = Usuarios.query.order_by(Usuarios.fecha_registro).all()
+    user_list = []
+    target_timezone = pytz.timezone('America/Argentina/Buenos_Aires') 
+    for user in entries:
+        # Convertir y formatear la fecha de registro
+        if user.fecha_registro:
+            utc_date = user.fecha_registro.replace(tzinfo=pytz.utc)
+            localized_date = utc_date.astimezone(target_timezone)
+            fecha_formateada = localized_date.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            fecha_formateada = None
         
-        return jsonify(user_list)
-        # return jsonify([{'id': entry.id, 'nombre': entry.nombre, 'monedas': entry.monedas, 'fecha_registro': entry.fecha_registro} for entry in entries]), 200
+        user_data = {
+            'id': user.id,
+            'nombre': user.nombre,
+            'monedas': user.monedas,
+            'fecha_registro': fecha_formateada
+        }
+        user_list.append(user_data)
+    
+    return jsonify(user_list)
+
+
+@app.route('/perfiles/<int:id>', methods=['DELETE'])
+def eliminar_usuario(id):
+    try:
+        usuario = Usuarios.query.get(id)
+        if usuario:
+            db.session.delete(usuario)
+            db.session.commit()
+            return jsonify({'mensaje': 'Usuario eliminado correctamente'}), 200
+        else:
+            return jsonify({'error': 'Usuario no encontrado'}), 404
+    except exc.SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': 'Error al eliminar usuario', 'detalles': str(e)}), 500
+
+
+
+@app.route('/perfiles/<int:id>', methods=['PUT'])
+def actualizar_usuario(id):
+    try:
+        usuario = Usuarios.query.get(id)
+        if usuario:
+            nuevo_nombre = request.json.get('nombre')
+            if nuevo_nombre:
+                usuario.nombre = nuevo_nombre
+                db.session.commit()
+                return jsonify({'mensaje': 'Nombre de usuario actualizado correctamente'}), 200
+            else:
+                return jsonify({'error': 'Falta el campo "nombre" en la solicitud'}), 400
+        else:
+            return jsonify({'error': 'Usuario no encontrado'}), 404
+    except exc.SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': 'Error al actualizar el nombre del usuario', 'detalles': str(e)}), 500
