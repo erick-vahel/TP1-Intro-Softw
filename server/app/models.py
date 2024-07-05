b27922622b503bce882a594b5bb464d8633c3e8b
Erick <npaiva@fi.uba.ar>
2024-07-05 17:55:50 -0300

    iniciado agregado de rutas para granjas

--- server/app/models.py
+++ server/app/models.py
@@ -9,3 +9,15 @@ class Usuarios(db.Model):
 
     def __repr__(self):
         return f'<ExampleModel {self.name}>'
+
+class Granjas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    fila = db.Column(db.Integer, nullable=False)
+    columna = db.Column(db.Integer, nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    usuario = db.relationship('Usuarios', backref=db.backref('granjas', lazy=True))
+
+
+    def __repr__(self):
+        return f'<ExampleModel {self.name}>'
