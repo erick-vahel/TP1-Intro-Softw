b71f16900723ab41c246940ab3aacddf0a575fb6
Erick <npaiva@fi.uba.ar>
2024-07-20 19:30:28 -0300

    agregada opcion para crear granja para cliente

--- client/src/components/Perfiles.jsx
+++ client/src/components/Perfiles.jsx
@@ -26,6 +26,31 @@ const Perfiles = () => {
     }
   };
 
+  const agregarGranja = async (userID)=>{
+    const apiUrl = `http://127.0.0.1:5000/granja/registro`; 
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({
+          id:userID
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en solicitud al crear granja');
+      }
+
+      // Aquí puedes manejar la respuesta si es necesario
+      console.log('Granja creada correctamente');
+    } catch (error) {
+      console.error('Error al crear granja:', error);
+    }
+}
+
+
   const eliminarUsuario = async (id)=>{
     const apiUrl = `http://127.0.0.1:5000/perfiles/${id}`; 
     try {
@@ -59,7 +84,8 @@ const Perfiles = () => {
                 <h4>{user.nombre}</h4>
                 <p>Monedas: {user.monedas}</p>
                 <p>Fecha de Registro: {user.fecha_registro}</p>
-                <Link className='btn btn-primary' to={`/registro/${user.id}?nombre=${user.nombre}`}>Modificar</Link>
+                <Link className='btn btn-primary' to={`/registro/${user.id}?nombre=${user.nombre}`}>Renombrar</Link>
+                <Button onClick={()=>agregarGranja(user.id)} variant="outline-secondary">Agregar granja</Button>
                 <Button onClick={()=>eliminarUsuario(user.id)} variant="outline-danger">Eliminar</Button>
               </div>
             </div>
--- server/README.md
+++ server/README.md
@@ -1,10 +1,20 @@
 - crear entorno
-1. python -m venv venv
+    1. python -m venv venv
 
 - inicializar
-1. source venv/bin/activate
-1. pip install -r requirements.txt
-1. deactivate
+    1. source venv/bin/activate
+    1. pip install -r requirements.txt
+    1. deactivate
 
 - para ingresar a db
-1. psql -U erick_db -d juego -W
+    1. psql -U erick_db -d juego -W
+
+
+- join by id
+
+    1. `SELECT u.id AS usuario_id, u.nombre AS nombre_usuario, u.monedas, u.fecha_registro AS fecha_registro_usuario,
+        g.id AS granja_id, g.fecha_registro AS fecha_registro_granja
+    FROM usuarios u
+    JOIN granjas g ON u.id = g.usuario_id
+    WHERE u.id = 1;  -- Aquí reemplaza 1 con el id del usuario específico que quieres consultar
+    `
