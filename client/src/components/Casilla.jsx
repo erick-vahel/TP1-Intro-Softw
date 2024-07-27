b75b508d08f72b3d293362b0b7fc2e2fb3b9e861
Erick <npaiva@fi.uba.ar>
2024-07-27 16:41:04 -0300

    iconos en casillas para opciones de cultivo

--- client/src/components/Casilla.jsx
+++ client/src/components/Casilla.jsx
@@ -1,14 +1,37 @@
-import Col from "react-bootstrap/Col";
 import "./Casilla.css"
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
+
+const IMG_OPC_CULT_1 = "https://cdn-icons-png.flaticon.com/512/1514/1514935.png";
+const IMG_OPC_CULT_2 = "https://cdn-icons-png.flaticon.com/512/424/424227.png";
+const IMG_OPC_CULT_3 = "https://cdn-icons-png.freepik.com/512/6532/6532538.png";
+
+const OPC_CULTIVOS = [
+  IMG_OPC_CULT_1,
+  IMG_OPC_CULT_2,
+  IMG_OPC_CULT_3
+]
 
 
 function Casilla() {
-    return (
-        <>
-            <Col className='casilla'>opc</Col>
-        </>
-    );
+  const [opcActual, setOpcActual] = useState(2);
+
+  /**
+   * Muestra la siguiente opcion para cultivar
+   * @param {HTMLButtonElement} btn casilla en la granja a modificar
+   */
+  function mostrarSiguiente() {
+    opcActual<(OPC_CULTIVOS.length-1)?setOpcActual((prevState)=>++prevState):setOpcActual(0);
+  }
+
+  return (
+    <>
+        <Button onClick={mostrarSiguiente} className='casilla' variant=" btn-outline-warning">
+          <img className="cultivo-icono" src={OPC_CULTIVOS[opcActual]} alt="opc-img-cultivo" />
+        </Button>
+    </>
+  );
 }
 
 
